986cc365a0dfb4626adb72c49ccac08981280037
Hemasen1412 <47857438+Hemasen1412@users.noreply.github.com>
2019-08-04 13:01:07 +0530

    Create vowelornot.c

---
+++ vowelornot.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+ char c=a;
+ if(c=='a' || c=='e' || c=='i' || a=='o' ||c=='u' ||c=='A' || c=='E'||c=='I'||c=='O'||c=='U')
+  printf("Vowel");
+ else
+  printf("Consonents");
+ getch();
+}
